1898b2e13d9813e03d467e90ee22cf10ccdb5356
pritam hooda <hoodap@sheridancollege.ca>
2023-01-28 22:14:44 -0500

    added validation for taco and taco order, so user can only pass in valid info into the html forms

--- taco-cloud/taco-cloud/src/main/java/com/example/tacos/TacoOrder.java
+++ taco-cloud/taco-cloud/src/main/java/com/example/tacos/TacoOrder.java
@@ -1,19 +1,31 @@
 package com.example.tacos;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class TacoOrder {
+    @NotBlank(message = "Delivery name is required")
     private String deliveryName;
+    @NotBlank(message = "Delivery street is required")
     private String deliveryStreet;
+    @NotBlank(message = "Delivery city is required")
     private String deliveryCity;
+    @NotBlank(message = "Delivery province is required")
     private String deliveryProvince;
+    @Pattern(regexp = "/^[A-Za-z]\\d[A-Za-z][ -]?\\d[A-Za-z]\\d$/", message = "Postal code is required and formatted correctly.")
     private String deliveryPostalCode;
+    @CreditCardNumber(message = "Must be a valid credit card number")
     private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message = "Date must be formatted MM/YY")
     private String ccExpiration;
+    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
     private List<Taco> tacos = new ArrayList<>();
 
